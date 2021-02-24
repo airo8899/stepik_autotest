6357dea2efbf26fbbfd22ed03652dfecc16db4e7
airo <diykovaa@gmail.com>
2021-02-24 20:00:12 +0300

    4.3.14 final

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,8 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        login_link = self.browser.find_element_by_css_selector("#login_link")
-        login_link.click()
+    pass
 
-    def should_be_login_link(self):
-        assert self.is_element_present("#login_link_invalid"), "Login link is not presented"
+
